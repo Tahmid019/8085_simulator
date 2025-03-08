6fc81109ffeb79ccc4db89bc0ecd5ad4f1712fee
Tahmid Ul Haque Choudhury <tahmidchoudhury019@gmail.com>
2025-03-08 19:48:01 +0530

    feat: Instruction Map

---
+++ Instructions.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <unordered_map>
+#include <functional>
+
+class Instruction {
+public:
+	std::string mnemonic;
+	uint8_t opcode;
+	int wordSize;
+
+	Instruction(std::string mn, uint8_t oc, int ws) : mnemonic(mn), opcode(oc), wordSize(ws) {}
+	static void Execute(std::string mnemonic);
+};
+
+extern std::unordered_map<std::string, Instruction> instructionSet;
+extern std::unordered_map<std::string, std::function<void()>> instructionHandlers;
