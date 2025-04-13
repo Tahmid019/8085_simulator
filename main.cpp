43a9426ac7c1d5964beed07165c8100b42675ee4
Tahmid Ul Haque Choudhury <tahmidchoudhury019@gmail.com>
2025-04-13 10:10:18 +0530

    Test: 8 bit div

--- main.cpp
+++ main.cpp
@@ -44,8 +44,8 @@ public:
 
 int main() {
 
-    uint16_t start_addr = 0x1ff5;
-    string filename = "mul.txt";
+    uint16_t start_addr = 0x1ff8;
+    string filename = "div8.txt";
 
     sim_8085 instance;
     instance.load_program(filename, start_addr, false);
